73c3d692a01044aad91a315ff63ac8b9f8f5806d
Frans Huang <franssiswanto@gmail.com>
2021-01-11 12:38:12 +0700

    - frontend is able to change `loginType` for each role

--- frontend/src/pages/ModeratorPage/components/PinSettings/index.js
+++ frontend/src/pages/ModeratorPage/components/PinSettings/index.js
@@ -2,13 +2,21 @@
 import React from "react";
 import { useSettings } from "../SettingsProvider";
 
+import LoginTypeDropdown from "../LoginTypeDropdown";
 import TextInput from "components/TextInput";
+import { Grid } from "@material-ui/core";
 
 function PinSettings() {
   const {
     presenterPin,
     participantPin,
     moderatorPin,
+    participantLoginType,
+    setParticipantLoginType,
+    presenterLoginType,
+    setPresenterLoginType,
+    moderatorLoginType,
+    setModeratorLoginType,
     setPresenterPin,
     setParticipantPin,
     setModeratorPin
@@ -21,32 +29,62 @@ function PinSettings() {
         <strong>Note: </strong>
         Leave it blank if you don't wish to change PIN.
       </p>
-      <TextInput 
-        label="Presenter PIN"
-        text={presenterPin}
-        onChange={setPresenterPin}
-        type="password"
-        placeholder="Enter new pin here"
-        autoComplete="off"
-      />
+      <Grid spacing={2} container>
+        <Grid xs={4} item>
+          <LoginTypeDropdown
+            value={presenterLoginType}
+            onChange={setPresenterLoginType}
+          />
+        </Grid>
+        <Grid xs item>
+          <TextInput 
+            label="Presenter PIN"
+            text={presenterPin}
+            onChange={setPresenterPin}
+            type="text"
+            placeholder="Enter new pin here"
+            autoComplete="off"
+          />
+        </Grid>
+      </Grid>
+      
+      <Grid spacing={2} container>
+        <Grid xs={4} item>
+          <LoginTypeDropdown
+            value={moderatorLoginType}
+            onChange={setModeratorLoginType}
+          />
+        </Grid>
+        <Grid xs item>
+          <TextInput 
+            label="Moderator PIN"
+            text={moderatorPin}
+            onChange={setModeratorPin}
+            type="text"
+            placeholder="Enter new pin here"
+            autoComplete="off"
+          />
+        </Grid>
+      </Grid>
 
-      <TextInput 
-        label="Moderator PIN"
-        text={moderatorPin}
-        onChange={setModeratorPin}
-        type="password"
-        placeholder="Enter new pin here"
-        autoComplete="off"
-      />
-
-      <TextInput 
-        label="Participant PIN"
-        text={participantPin}
-        onChange={setParticipantPin}
-        type="password"
-        placeholder="Enter new pin here"
-        autoComplete="off"
-      />
+      <Grid spacing={2} container>
+        <Grid xs={4} item>
+          <LoginTypeDropdown
+            value={participantLoginType}
+            onChange={setParticipantLoginType}
+          />
+        </Grid>
+        <Grid xs item>
+          <TextInput 
+            label="Participant PIN"
+            text={participantPin}
+            onChange={setParticipantPin}
+            type="text"
+            placeholder="Enter new pin here"
+            autoComplete="off"
+          />
+        </Grid>
+      </Grid>
     </>
   )
 }
