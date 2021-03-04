c0c692003876282c900a51dc1e27d8495fca7a64
Frans Huang <franssiswanto@gmail.com>
2021-03-04 13:29:36 +0700

    Forget to the the configuraiton file

---
+++ backend/src/config/configuration.ts
@@ -0,0 +1,27 @@
+import PinConfiguration from "./pin";
+
+const config = {
+  default: {
+    presenter: {
+      login_type: "sso",
+      pin: PinConfiguration.presenter
+    },
+    participant: {
+      login_type: "sso",
+      pin: PinConfiguration.participant,
+      raise_hand: true
+    },
+    moderator: {
+      login_type: "sso",
+      pin: PinConfiguration.moderator
+    },
+    tabs: {
+      questions: true,
+      chat: true,
+      participants: true,
+      polling: true
+    }
+  }
+}
+
+export default config;
