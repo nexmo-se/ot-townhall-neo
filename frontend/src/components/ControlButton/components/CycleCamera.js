a71a59a44a4b1bfe837b0d5770c890eec8d8bd19
Frans Huang <franssiswanto@gmail.com>
2021-03-16 12:53:25 +0700

    What I did - Refactor some code to make it pretty and scalable - Townhall now support cycleVideo

---
+++ frontend/src/components/ControlButton/components/CycleCamera.js
@@ -0,0 +1,33 @@
+// @flow
+import React from "react";
+import { BaseProps } from "../types";
+import { Publisher } from "@opentok/client";
+
+import FlipCameraIosIcon from '@material-ui/icons/FlipCameraIos';
+import ControlButton from "../index";
+
+interface CycleCameraProps extends BaseProps {
+  publisher: Publisher;
+}
+
+function CycleCameraButton ({ publisher, ...props }: CycleCameraProps) {
+  function handleClick () {
+    try {
+      publisher.cycleVideo();
+    } catch (err) {
+      alert("You might only have one camera or something went wrong")
+    }
+  }
+
+  return (
+    <ControlButton
+      {...props}
+      onClick={handleClick}
+      forceColor="Vlt-bg-purple-dark"
+    >
+      <FlipCameraIosIcon fontSize="inherit" />
+    </ControlButton>
+  )
+}
+
+export default CycleCameraButton;
