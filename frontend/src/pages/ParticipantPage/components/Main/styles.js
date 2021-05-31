9960ae75480f0aebd11732be7c0af220fda7ff15
Frans Huang <franssiswanto@gmail.com>
2021-05-31 17:48:37 +0800

    Fix styling

--- frontend/src/pages/ParticipantPage/components/Main/styles.js
+++ frontend/src/pages/ParticipantPage/components/Main/styles.js
@@ -1,35 +1,38 @@
 // @flow
 import { makeStyles } from "@material-ui/styles";
-export default makeStyles(() => ({
-  container: { 
-    width: "100%", 
-    flex: 1,
-    overflow: "hidden",
-    display: "flex", 
-    flexDirection: "row" 
-  },
-  leftContainer: { 
-    flexBasis: "75%",
-    overflow: "hidden",
-    flexDirection: "column",
-    display: "flex",
-    position: "relative" 
-  },
-  black: { backgroundColor: "black" }, 
-  logoContainer: { 
-    display: "flex", 
-    flexDirection: "column", 
-    position: "absolute", 
-    top: 32, 
-    right: 32, 
-    zIndex: 2, 
-    justifyContent: "center", 
-    alignItems: "flex-end"
-  },
-  vonageLogo: {
-    position: "absolute",
-    bottom: 32,
-    right: 32,
-    zIndex: 2
-  }
-}))
+export default makeStyles(
+  () => ({
+    container: { 
+      width: "100%", 
+      flex: 1,
+      overflow: "hidden",
+      display: "flex", 
+      flexDirection: "row" 
+    },
+    leftContainer: { 
+      flexBasis: "75%",
+      overflow: "hidden",
+      flexDirection: "column",
+      display: "flex",
+      position: "relative" 
+    },
+    black: { backgroundColor: "black" }, 
+    logoContainer: { 
+      display: "flex", 
+      flexDirection: "column", 
+      position: "absolute", 
+      top: 32, 
+      right: 32, 
+      zIndex: 2, 
+      justifyContent: "center", 
+      alignItems: "flex-end"
+    },
+    vonageLogo: {
+      position: "absolute",
+      bottom: 32,
+      right: 32,
+      zIndex: 2
+    }
+  }),
+  { index: 1 }
+)
