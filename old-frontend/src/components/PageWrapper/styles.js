01239a69a75a27e3aec1cfd07150d24d6f4103fd
Beejay <beejay.urzo@gmail.com>
2026-06-30 19:25:10 +0800

    Remove old-frontend from tracking and add to .gitignore

--- old-frontend/src/components/PageWrapper/styles.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import { makeStyles } from "@material-ui/core/styles";
-export default makeStyles(() => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    height: "100%"
-  }
-}), { index: 1 });
--- old-frontend/src/pages/ModeratorPage/components/PollingModal/styles.js
+++
@@ -1,5 +0,0 @@
-// @flow
-import { makeStyles } from "@material-ui/styles";
-export default makeStyles(() => ({
-  pointer: { cursor: "pointer" }
-}), { index: 1 })
